62900ba3ed44b38aaa84f09b3ab6ab82e22e4e51
vladislavK3309 <vladkisluhin3309@gmail.com>
2019-09-15 19:59:05 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header/Header";
+import NavBar from "./components/Nav/Nav";
+import Profile from "./components/Profile/Profile";
+import Dialogs from "./components/Dialogs/Dialogs";
+import News from "./components/Nav/News/News";
+import Music from "./components/Nav/Music/Music";
+import Settings from "./components/Nav/Settings/Settings";
+import Route from "react-router-dom/es/Route";
+import {BrowserRouter} from "react-router-dom";
+import FriendList from "./components/Nav/Friends/FriendList/FriendList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App(props) {
+    return (
+        <BrowserRouter>
+            <div className='app-wrapper'>
+                <Header/>
+                <NavBar/>
+                <div className='app-wrapper-content'>
+                    <Route path='/dialogs' render={() =>
+                        <Dialogs state={props.state.dialogsPage}/>}/>
+                    <Route path='/profile' render={() =>
+                        <Profile state={props.state.profilePage}
+                                dispatch = {props.dispatch}/>}/>
+                    <Route path='/news' render={() => <News/>}/>
+                    <Route path='/music' render={() => <Music/>}/>
+                    <Route path='/settings' render={() => <Settings/>}/>
+                </div>
+                <FriendList state={props.state.sideBar}/>
+            </div>
+        </BrowserRouter>
+    );
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
+import * as serviceWorker from './serviceWorker';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {BrowserRouter} from "react-router-dom";
+import {store} from "./Redux/State";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rerenderEntireTree = (state) => {
+    ReactDOM.render(
+        <BrowserRouter>
+            <App state={store._state} dispatch = {store.dispatch.bind(store)}/>
+        </BrowserRouter>,
+        document.getElementById('root'));
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+rerenderEntireTree(store.getState());
+
+store.subscribe(rerenderEntireTree);
+
+
+
+
+
+
 serviceWorker.unregister();
